8521514e2ad90de79c02b93c4e43c6d585267041
devThapaMagar <devmagar@getmessiah.io>
2022-07-18 15:19:00 +0930

    [FIX] issues regarding google global tag manager.

--- pages/index.js
+++ pages/index.js
@@ -25,14 +25,18 @@ export default function Home() {
       <Head>
         <meta name="google-site-verification" content="p2uwWUVVnSU7UktV3DQ0SU7mXrfCjgbJ5uPteivBI00" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10950125848"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);
-          }
-          gtag('js', new Date());
-
-          gtag('config', 'AW-10950125848');
-        </script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            
+            gtag('config', 'AW-10950125848');
+            `,
+          }}
+        />
 
       </Head>
 
