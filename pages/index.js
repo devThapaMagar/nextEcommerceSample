2949f5f5f1232cc0a12eb29eb0615f2a20fdf6cc
devThapaMagar <devmagar@getmessiah.io>
2022-07-18 15:11:35 +0930

    [ADD] google global site tag

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,16 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <meta name="google-site-verification" content="p2uwWUVVnSU7UktV3DQ0SU7mXrfCjgbJ5uPteivBI00" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10950125848"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);
+          }
+          gtag('js', new Date());
+
+          gtag('config', 'AW-10950125848');
+        </script>
+
       </Head>
 
       <main className={styles.main}>
